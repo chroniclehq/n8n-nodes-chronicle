7b4ef8e8871ad148bd0e9d21562412c5ac3b2fe6
tushar-nath <tusharnath10@gmail.com>
2026-05-15 19:39:14 +0530

    chore: scaffold n8n community node package

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+	semi: true,
+	trailingComma: 'all',
+	bracketSpacing: true,
+	useTabs: true,
+	tabWidth: 2,
+	arrowParens: 'always',
+	singleQuote: true,
+	endOfLine: 'lf',
+	printWidth: 100,
+};
